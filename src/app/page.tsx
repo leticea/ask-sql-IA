9d4fa656b4f853f595c698edff2fd29c97f028aa
leticea <nascimento.leticeah@gmail.com>
2023-08-17 18:42:19 -0300

    feat(page.tsx): editing styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Trash2, Stars } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="max-w-[430px] mx-auto pt-12 pb-4">
+    <div className="max-w-[430px] mx-auto px-4 pt-12 pb-4">
       <header className="flex items-center justify-between">
         <Image src={logoImage} alt="" />
 
@@ -14,29 +14,39 @@ export default function Home() {
       </header>
 
       <form className="py-8 w-full flex flex-col text-foam">
-        <label htmlFor="schema">Cole o seu código SQL aqui:</label>
+        <label htmlFor="schema" className="text-lg font-light">
+          Cole o seu código SQL aqui:
+        </label>
         <textarea
           name="schema"
           id="schema"
           className="my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-1 focus:ring-lime-600"
         />
 
-        <label htmlFor="question">Faça uma pergunta sobre o código:</label>
+        <label htmlFor="question" className="text-lg font-light">
+          Faça uma pergunta sobre o código:
+        </label>
         <textarea
           name="question"
           id="question"
           className="my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-1 focus:ring-lime-600"
         />
 
-        <button type="submit" className="text-pistachio">
+        <button
+          type="submit"
+          className="text-pistachio flex items-center justify-center rounded-lg border boder-pistachio h-14 gap-2"
+        >
           <Stars className="w-6 h-6" />
           Perguntar à inteligência artificial
         </button>
       </form>
 
-      <div>
-        <span>Resposta:</span>
-        <textarea className="my-4" />
+      <div className="mt-6">
+        <span className="text-lg font-light text-foam">Resposta:</span>
+        <textarea
+          readOnly
+          className="my-4 w-full bg-transparent border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-1 focus:ring-lime-600"
+        />
       </div>
     </div>
   );
