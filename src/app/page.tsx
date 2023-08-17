cc3d451030e9755c9384b925582375ed0c864f9a
leticea <nascimento.leticeah@gmail.com>
2023-08-17 17:33:21 -0300

    feat(page.tsx): editing header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import logoImage from "../assets/logo.svg";
+import { Trash2 } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <header>
+    <div className="max-w-[430px] mx-auto pt-12 pb-4">
+      <header className="flex items-center justify-between">
         <Image src={logoImage} alt="" />
+
+        <button>
+          <Trash2 className="h-8 w-8 text-snow" strokeWidth={0.8} />
+        </button>
       </header>
 
       <form action=""></form>
